bf8bf05e9a71f793fce75a50b26ddc03ce637b97
urcousinveer <vibhuchopra8@gmail.com>
2024-11-14 21:04:52 -0800

    Added integer sorting feature

---
+++ IntegerSorter.java
@@ -0,0 +1,9 @@
+import java.util.Collections;
+import java.util.List;
+
+public class IntegerSorter {
+    public static void sortIntegers(List<Integer> integers) {
+        Collections.sort(integers);
+        System.out.println("Sorted Integers: " + integers);
+    }
+}
